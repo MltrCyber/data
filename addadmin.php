66c346e7a717f5aece1ff16b42c405cc11ee15b9
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2023-10-24 10:45:38 +0700

    Update addadmin.php

--- addadmin.php
+++ addadmin.php
@@ -1,31 +1,22 @@
 <?php
-    error_reporting(E_ALL);
-$host="localhost"; 
-$user="sakurat2_db"; 
-$password="sakurat2_db"; 
-$database="sakurat2_db";
-$conn=mysqli_connect($host,$user,$password,$database) or die(mysqli_error());
-//cek koneksi  
-if($conn){ 
-echo "berhasil koneksi"; 
-}else{ 
-echo "gagal koneksi"; 
-} 
-   
-        $userku = "jokowi";
-        $id = "5";
-        $pass = '$2y$10$OVRTFcwKLk13TqInDOTIDOklEEUNwCoNp0M6ilQaTvZeJgtLwOXvG';
-        $status = "superadmin";
-        $info = "1";
-    
-        $query = "INSERT INTO 'tb_user' (user ,pass,level,status) VALUES('jokowi', '$pass','$status','$info')";
-        $result = mysqli_query($conn, $query);
-        if($result){
-            $msg = "Registered Sussecfully";
-            echo $msg;
-        }
-        else
-            $msg = "Error Registering";
-            echo $msg;
-    
+$conn =new mysqli('localhost', 'sakurat2_db', 'sakurat2_db' , 'sakurat2_db');
+
+$query = '';
+$sqlScript = file('sakurat2_db.sql');
+foreach ($sqlScript as $line)	{
+	
+	$startWith = substr(trim($line), 0 ,2);
+	$endWith = substr(trim($line), -1 ,1);
+	
+	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+		continue;
+	}
+		
+	$query = $query . $line;
+	if ($endWith == ';') {
+		mysqli_query($conn,$query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query. '</b></div>');
+		$query= '';		
+	}
+}
+echo '<div class="success-response sql-import-response">SQL file imported successfully</div>';
 ?>
