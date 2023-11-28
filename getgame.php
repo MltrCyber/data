464ed68fcf4ce7ace73026c345d2bc821367a6a3
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2023-11-28 19:27:52 +0700

    Create getgame.php

---
+++ getgame.php
@@ -0,0 +1,41 @@
+<?php 
+require '../config/koneksi.php';
+$sql_5 = mysqli_query($conn,"SELECT * FROM `tb_tripayapi` WHERE cuid = 4") or die(mysqli_error());
+				$s5 = mysqli_fetch_array($sql_5);
+				$merchantCodes = $s5['merchant_code'];
+				$apiKey = $s5['api_key'];
+$signe = $merchantCodes.$apiKey;
+        $sign = md5($signe);
+        $order = "order";
+        $service = "ML13_1-S10";
+        $quantity = "1000";
+        $data_no = "370028836";
+        $data_zone = "9698";
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://vip-reseller.co.id/api/game-feature',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => array(
+              'key' => $apiKey,
+              'sign' => $sign,
+              'type' => $order,
+              'service' => $service,
+             // 'quantity' => $quantity,
+              'data_no' => $data_no,
+              'data_zone' => $data_zone
+              
+          ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+ ?>
